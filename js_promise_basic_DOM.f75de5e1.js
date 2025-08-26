0ed06411f341dee6c5edf30caa8d10a2f11ecb5f
Marta Osikiewicz <marta.osikiewicz@gmail.com>
2025-08-26 17:27:15 +0200

    make build

---
+++ js_promise_basic_DOM.f75de5e1.js
@@ -0,0 +1,40 @@
+'use strict';
+const promise1 = new Promise((resolve, reject)=>{
+    const logo = document.querySelector('.logo');
+    if (!logo) {
+        reject(new Error('Brak elementu .logo'));
+        return;
+    }
+    logo.addEventListener('click', ()=>{
+        resolve();
+    });
+});
+const promise2 = new Promise((resolve, reject)=>{
+    setTimeout(()=>{
+        reject(new Error("Min\u0119\u0142y 3 sekundy!"));
+    }, 3000);
+});
+promise1.then(()=>{
+    const div = document.createElement('div');
+    div.className = 'message';
+    div.textContent = 'Promise was resolved!';
+    document.body.appendChild(div);
+}).catch(()=>{
+    const div = document.createElement('div');
+    div.className = 'message error-message';
+    div.textContent = 'Promise was rejected!';
+    document.body.appendChild(div);
+});
+promise2.then(()=>{
+    const div = document.createElement('div');
+    div.className = 'message';
+    div.textContent = 'Promise was resolved!';
+    document.body.appendChild(div);
+}).catch(()=>{
+    const div = document.createElement('div');
+    div.className = 'message error-message';
+    div.textContent = 'Promise was rejected!';
+    document.body.appendChild(div);
+});
+
+//# sourceMappingURL=js_promise_basic_DOM.f75de5e1.js.map
